0a79e60d3c26ace5383ef02b8eb373cdf6920466
jguillevic <guillevicjerome@yahoo.fr>
2023-05-17 17:29:02 +0200

    Avancées sur la calculette de prime (visuel)

--- Controller/Calculator/PremiumTax/PremiumTaxCalculatorController.php
+++ Controller/Calculator/PremiumTax/PremiumTaxCalculatorController.php
@@ -14,6 +14,48 @@ class PremiumTaxCalculatorController
 		return $view->Render();
 	}
 
+	public function GetJsonCalcResult($queryParameters)
+	{
+		if ($_SERVER['REQUEST_METHOD'] === 'POST')
+		{
+			// Récupération des infos.
+			$grossAmount = (float)$queryParameters['gross-amount']->GetValue();
+			$marginalTaxBracket = (float)$queryParameters['marginal-tax-bracket']->GetValue();
+
+			// Taux de csg.
+			$csgRate = 0.092;
+			// Taux de csg déductible.
+			$deductibleCsgRate = 0.068;
+			// Taux de crds.
+			$crdsRate = 0.005;
+			// Taux d'abattemment des frais professionnels.
+			$professionalFeesDeduction = 0.1;
+
+			// Montant de la csg + crds.
+			$csgAndCrdsAmount = $grossAmount * ($csgRate + $crdsRate);
+			$netAmount = $grossAmount - $csgAndCrdsAmount;
+			$netTaxableAmount = $grossAmount * (1 - $deductibleCsgRate);
+			$incomeTax = $netTaxableAmount * (1 - $professionalFeesDeduction) * $marginalTaxBracket;
+
+			$path = PathHelper::GetPath([ "Calculator", "PremiumTax", "DisplayCalcResult" ]);
+			$view = new View($path);
+			return json_encode([
+				'grossAmount' => $grossAmount
+				, 'marginalTaxBracket' => $marginalTaxBracket
+				, 'csgRate' => $csgRate
+				, 'deductibleCsgRate' => $deductibleCsgRate
+				, 'crdsRate' => $crdsRate
+				, 'professionalFeesDeduction' => $professionalFeesDeduction
+				, 'csgAndCrdsAmount' => round($csgAndCrdsAmount, 2)
+				, 'netAmount' => round($netAmount, 2)
+				, 'netTaxableAmount' => round($netTaxableAmount, 2)
+				, 'incomeTax' => round($incomeTax, 2)
+			]);
+		}
+
+		return -1;
+	}
+
 	public function DisplayCalcResult($queryParameters)
 	{
 		if ($_SERVER['REQUEST_METHOD'] === 'POST')
